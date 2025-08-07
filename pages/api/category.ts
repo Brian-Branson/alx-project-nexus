226319a54d4ab20e25437c1b8cbfdb6bd193b584
Brian <karanjabrian382@gmail.com>
2025-08-07 19:41:00 +0300

    done

--- pages/api/category.ts
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  try {
-      const response = await fetch(`https://alx-project-nexus-psi.vercel.app/api/v1/products/?category=${category_slug}`);
-      const data = await response.json();
-      res.status(200).json(data);
-    } catch (error) {
-    res.status(500).json({ error: "Failed to fetch products." });
-  }
-}
